9141ca7c25f1b612567c55e6dea7e56f79c4b1bd
thedumpstertruck2308 <joshfindsdiamondback@gmail.com>
2026-09-21 20:38:01 -0600

    Update jewternal.lua

--- jewternal.lua
+++ jewternal.lua
@@ -315,7 +315,6 @@ shared['Prosper'] = {
                 },
             },
 
-            ['Use Weapon Cooldown'] = false, -- Will use your weapon cooldown to delay your triggerbot.
 
             ['Weapon Delays'] = { -- You can put any weapon in their proper category, if the weapon you're using isn't in here, it acts as 0 delay.
                 ['Shotguns'] = {
